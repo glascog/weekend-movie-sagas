94aa49c8fbf5986ad900287aa2c9603b966ff964
Gabe Glasco <gabeglasco@gmail.com>
2023-08-27 11:06:40 -0500

    add server side genres get request

--- server/routes/genre.router.js
+++ server/routes/genre.router.js
@@ -4,7 +4,15 @@ const pool = require('../modules/pool')
 
 router.get('/', (req, res) => {
   // Add query to get all genres
-  res.sendStatus(500)
-});
+  console.log('Inside server side genres get')
+  const queryText = 'SELECT * FROM genres';
+  pool.query(queryText)
+    .then((result) => {res.send(result.rows);
+    })
+    .catch((error) => {
+      console.log('Error completing genres query', error)
+      res.sendStatus(500);
+    })  
+  });
 
 module.exports = router;
