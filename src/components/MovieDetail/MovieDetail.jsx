793d1e9a1051b3f721023fe55df8e3fb4e58f48c
Gabe Glasco <gabeglasco@gmail.com>
2023-08-28 00:19:17 -0500

    add styling to movie detail

--- src/components/MovieDetail/MovieDetail.jsx
+++ src/components/MovieDetail/MovieDetail.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom/cjs/react-router-dom.min';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import Typography from '@mui/material/Typography';
@@ -25,18 +26,32 @@ function MovieDetail() {
 
     return (
         <>
-        <Card sx={{ display: "inline-flex",
-                    flexDirection: "row"}}>
+        <Card sx={{ display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    padding: "20px",
+                    boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
+                    borderRadius: "8px"
+                 }}>
             <CardHeader 
                 component = {Typography}
                 title = {movie.title}
                 subheader = {genre.genres}
             >
             </CardHeader>
-                <CardMedia component="img" image={movie.poster} alt={movie.title}>
-                    {/* <img src={movie.poster} alt={movie.title}/> */}
+                {movie.description}
+
+                <CardMedia  component="img" 
+                            image={movie.poster} 
+                            alt={movie.title}
+                            sx={{width: "200px",
+                                height: "300px",
+                                objectFit: "cover"}}
+                >
                 </CardMedia>
+                <Link to={'/'}>
                 <Button>Back to Movies List</Button>
+                </Link>
         </Card>
 
         
